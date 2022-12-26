6d540317e7c481fb67584c07149fd92781292aef
mahdi kouhestani <kohestanimahdi@gmail.com>
2022-12-26 17:55:10 +0330

    add weather service client

---
+++ Neredataltics.SmartFeatures/Services/WeatherServices/WeatherService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+
+namespace Neredataltics.SmartFeatures.Services.WeatherServices
+{
+    public class WeatherService
+    {
+        private readonly Shared.WeatherService.WeatherServiceClient _weatherServiceClient;
+        public WeatherService(GrpcChannel grpcChannel)
+        {
+            _weatherServiceClient = new Shared.WeatherService.WeatherServiceClient(grpcChannel);
+        }
+    }
+}
