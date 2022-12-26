3cc278cc4b585018fd7740a3dc53cf5f0c7876dd
mahdi kouhestani <kohestanimahdi@gmail.com>
2022-12-26 17:35:54 +0330

    add smart model to create grpc server to get the weather for specific location

---
+++ Neredataltics.SmartModel/Services/WeatherService.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+
+namespace Neredataltics.SmartModel.Services
+{
+    public class WeatherService : SmartModel.WeatherService.WeatherServiceBase
+    {
+        static Random _random = new();
+        private int GetRandomTemperature() => _random.Next(-100, 100);
+        private AirConditions GetRandomAirConditions() => (AirConditions)_random.Next(4);
+
+        public override Task<WeatherConditionResponse> GetWeatherForLocation(WeatherConditionRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new WeatherConditionResponse
+            {
+                Condition = GetRandomAirConditions(),
+                Temperature = GetRandomTemperature(),
+            });
+        }
+    }
+}
